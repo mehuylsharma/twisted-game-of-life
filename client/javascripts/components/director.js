ee198291d42f8b0b62bac023c98cbde8ecdcfe26
Mehul Sharma <the8bitmehul@gmail.com>
2022-06-15 08:17:36 +0530

    Added user input functionality to change game of life board

--- client/javascripts/components/director.js
+++ client/javascripts/components/director.js
@@ -1,48 +1,56 @@
-const CELL_SIZE = 25;
-const GAME_SPEED = 0.5;
+var GAME_SPEED = 0.1;
 
 class Director {
   constructor() {
-    //What it will take?
+    this.running = false;
+  }
+
+  start() {
+    this.running = true;
+  }
+
+  stop() {
+    this.running = false;
   }
 
   run() {
-    for (let i = 0; i < ROW_SIZE; i++) {
-      for (let j = 0; j < COLUMN_SIZE; j++) {
-        //Rule #1: Each dead cell adjacent to exactly three live neighbors will become live in the next generation
-        let neighbor_count = 0;
-        if (this.exists(i-1,j)) { neighbor_count += 1; }
-        if (this.exists(i+1,j)) { neighbor_count += 1; }
-        if (this.exists(i,j-1)) { neighbor_count += 1; }
-        if (this.exists(i,j+1)) { neighbor_count += 1; }
-        if (this.exists(i-1,j-1)) { neighbor_count += 1; }
-        if (this.exists(i-1,j+1)) { neighbor_count += 1; }
-        if (this.exists(i+1,j-1)) { neighbor_count += 1; }
-        if (this.exists(i+1,j+1)) { neighbor_count += 1; }
-
-        if (neighbor_count == 3 && ( this.exists(i, j) == false )) {
-          this.create(i, j);
+    if (this.running) {
+      for (let i = 0; i < ROW_SIZE; i++) {
+        for (let j = 0; j < COLUMN_SIZE; j++) {
+  
+          //Rule #1: Each dead cell adjacent to exactly three live neighbors will become live in the next generation
+          let neighbor_count = 0;
+          if (this.exists(i-1,j)) { neighbor_count += 1; }
+          if (this.exists(i+1,j)) { neighbor_count += 1; }
+          if (this.exists(i,j-1)) { neighbor_count += 1; }
+          if (this.exists(i,j+1)) { neighbor_count += 1; }
+          if (this.exists(i-1,j-1)) { neighbor_count += 1; }
+          if (this.exists(i-1,j+1)) { neighbor_count += 1; }
+          if (this.exists(i+1,j-1)) { neighbor_count += 1; }
+          if (this.exists(i+1,j+1)) { neighbor_count += 1; }
+  
+          if (neighbor_count == 3 && ( this.exists(i, j) == false )) {
+            this.create(i, j);
+          }
+  
+          //Rule #2: Each live cell with one or fewer live neighbors will die in the next generation
+  
+          if (neighbor_count <= 1) {
+            this.delete(i, j);
+          }
+  
+          //Rule #3: Each live cell with four or more live neighbors will die in the next generation
+  
+          if (neighbor_count >= 4) {
+            this.delete(i, j);
+          }
+  
+          //Rule #4: Each live cell with either two or three live neighbors will remain alive for the next generation. 
+  
+          //Do nothing.
         }
-
-        //Rule #2: Each live cell with one or fewer live neighbors will die in the next generation
-
-        if (neighbor_count <= 1) {
-          this.delete(i, j);
-        }
-
-        //Rule #3: Each live cell with four or more live neighbors will die in the next generation
-
-        if (neighbor_count >= 4) {
-          this.delete(i, j);
-        }
-
-        //Rule #4: Each live cell with either two or three live neighbors will remain alive for the next generation. 
-
-        //Do nothing.
       }
     }
-
-    setTimeout(() => this.run(), GAME_SPEED*1000);
   }
 
   exists(x, y) {
--- client/javascripts/sketch.js
+++ client/javascripts/sketch.js
@@ -1,11 +1,22 @@
-const ROW_SIZE = 32;
-const COLUMN_SIZE = 20;
+const ROW_ELEMENT = document.getElementsByClassName('block-rows');
+const COLUMN_ELEMENT = document.getElementsByClassName('block-columns');
+const GAME_SPEED_ELEMENT = document.getElementsByClassName('game-speed');
+const CANVAS_CONTAINER = document.getElementsByClassName('canvas-container');
+
+const MAX_CANVAS_WIDTH = 1000;
+const MAX_CANVAS_HEIGHT = 500;
+const CELL_COLOR = '#00FF41';
+
+var ROW_SIZE = 40;
+var COLUMN_SIZE = 20;
+var CELL_SIZE = 25;
 
 let cells = new Array(ROW_SIZE);
 let director = new Director();
 
 function setup() {
-  createCanvas(800, 500);
+  var myCanvas = createCanvas(ROW_SIZE*CELL_SIZE, COLUMN_SIZE*CELL_SIZE);
+  myCanvas.parent('p5-canvas');
   for (let j = 0; j < ROW_SIZE; j++) {
     cells[j] = new Array(COLUMN_SIZE).fill(false);
   }
@@ -17,13 +28,12 @@ function setup() {
       }
     }
   }
-  director.run();
 }
 
 function draw() {
   clear();
   noStroke();
-  fill(25);
+  fill(CELL_COLOR);
   cells.forEach(row => {
     row.forEach(cell => {
       if (cell !== false) {
@@ -31,4 +41,49 @@ function draw() {
       }
     })
   })
+}
+
+function updateTGOL() {
+  ROW_SIZE = parseInt(ROW_ELEMENT[0].value);
+  COLUMN_SIZE = parseInt(COLUMN_ELEMENT[0].value);
+  GAME_SPEED = parseFloat(GAME_SPEED_ELEMENT[0].value);
+
+  if (ROW_SIZE > COLUMN_SIZE ) {
+    CELL_SIZE = MAX_CANVAS_HEIGHT/ROW_SIZE;
+  } else {
+    CELL_SIZE = MAX_CANVAS_WIDTH/COLUMN_SIZE;
+  }
+
+  CANVAS_CONTAINER[0].style['background-size'] = `${CELL_SIZE}px ${CELL_SIZE}px`;
+  
+  cells = new Array(ROW_SIZE);
+
+  for (let j = 0; j < ROW_SIZE; j++) {
+    cells[j] = new Array(COLUMN_SIZE).fill(false);
+  }
+
+  for (let i = 0; i < ROW_SIZE; i++) {
+    for (let j = 0; j < COLUMN_SIZE; j++) {
+      if (Math.random() < 0.5) {
+        director.create(i, j);
+      }
+    }
+  }
+
+  resizeCanvas(ROW_SIZE*CELL_SIZE, COLUMN_SIZE*CELL_SIZE);
+}
+
+function startTGOL() {
+  updateTGOL();
+  if (!director.running) {
+    director.start();
+    runTGOL();
+  }
+}
+
+function runTGOL() {
+  director.run();
+  if (director.running) {
+    setTimeout(() => runTGOL(), GAME_SPEED*1000);
+  }
 }
